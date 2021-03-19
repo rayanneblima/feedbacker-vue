e42fc16201c7b5c24abe0280df75648889d3c65d
Rayanne <rayanne_lima2010@hotmail.com>
2021-03-19 18:58:29 -0300

    Dashboard - Fix Errors

--- dashboard/src/components/Icons/index.vue
+++ dashboard/src/components/Icons/index.vue
@@ -3,10 +3,9 @@
 </template>
 
 <script>
-import Loading from './Loading'
-import Copy from './Copy'
-import ChevronDown from './ChevronDown'
-
+import Loading from './Loading.vue'
+import Copy from './Copy.vue'
+import ChevronDown from './ChevronDown.vue'
 export default {
   components: { Loading, Copy, ChevronDown },
   props: {
